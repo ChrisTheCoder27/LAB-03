0a5b98a98de8fcbf404e8e4c3fa07c3cf6c90e97
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2024-10-11 15:58:14 -0400

    Added nav agents and matched animations with movement

---
+++ Assets/Scripts/navControl.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navControl : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent agent;
+    bool isWalking = true;
+    private Animator animator;
+    public GameObject lookTarget;
+    float rotateSpeed = 1f;
+
+    float mySliderValue;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void OnGUI()
+    {
+        // Adding a slider to change the speed of the animator
+        //GUI.Label(new Rect(0, 25, 40, 60), "Speed");
+
+        //mySliderValue = GUI.HorizontalSlider(new Rect(45, 25, 200, 60), mySliderValue, 0.0F, 1.0F);
+        //animator.speed = mySliderValue;
+        // Changing the agent's speed based on the slider value
+        //agent.speed = mySliderValue * 1.5f;
+    }
+
+    void Update()
+    {
+        if (isWalking)
+        {
+            agent.destination = target.transform.position;
+        }
+        else
+        {
+            agent.destination = transform.position;
+        }
+        RotateTowardsTarget();
+    }
+
+    void RotateTowardsTarget()
+    {
+        float stepSize = rotateSpeed * Time.deltaTime;
+
+        Vector3 targetDir = lookTarget.transform.position - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, stepSize, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            isWalking = false;
+            animator.SetTrigger("ATTACK");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            isWalking = true;
+            animator.SetTrigger("WALK");
+        }
+    }
+}
